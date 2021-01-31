d0b7933b1a2f95cd6866884439e00da7cf964153
{s-amano} <{amahaya0831@gmail.com}>
2021-01-31 17:45:24 +0900

    deleteを追加,vue側バグあり

--- front/fan-letter-front/src/components/subComponents/FanLetterList.vue
+++ front/fan-letter-front/src/components/subComponents/FanLetterList.vue
@@ -20,6 +20,41 @@
               <v-list-item-action-text
                 v-text="fanletter.post_at"
               ></v-list-item-action-text>
+              <v-dialog v-model="dialog" :retain-focus="false">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    class="mr-4"
+                    dark
+                    color="primary"
+                    v-bind="attrs"
+                    v-on="on"
+                    >削除</v-btn
+                  >
+                </template>
+                <v-card>
+                  <v-card-title class="headline grey lighten-2">
+                    本当に削除しますか？
+                  </v-card-title>
+                  <v-card-text>
+                    {{
+                      fanletter.from
+                    }}さんがくれたあなたへのファンレターを本当に削除しますか？
+                  </v-card-text>
+
+                  <v-divider></v-divider>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      color="primary"
+                      @click="
+                        deleteFanletter(fanletter.id, fanletter.text, index)
+                      "
+                    >
+                      削除
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
             </v-list-item-action>
           </v-list-item>
           <v-divider
@@ -37,6 +72,7 @@ import axios from "axios";
 export default {
   data: () => ({
     fanletters: [],
+    dialog: false,
   }),
   mounted() {
     axios
@@ -45,6 +81,26 @@ export default {
       )
       .then((response) => (this.fanletters = response.data))
       .catch((error) => console.log(error));
+  },
+  methods: {
+    deleteFanletter(id, text, index) {
+      const lambdaApi =
+        "https://0lg3gcph42.execute-api.ap-northeast-1.amazonaws.com/dev/delete/" +
+        id +
+        "/" +
+        text;
+
+      axios
+        .delete(lambdaApi)
+        .then(() => {
+          console.log("削除成功");
+          this.dialog = false;
+          this.fanletters.splice(index, 1);
+        })
+        .catch(() => {
+          console.log("失敗");
+        });
+    },
   },
 };
 </script>
